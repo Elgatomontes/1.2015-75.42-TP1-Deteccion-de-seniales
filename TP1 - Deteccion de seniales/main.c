b3074fc69331dab1d2f4992ff6dbaea687a51b54
Gastón Montes <gaston.montes@hinch.as>
2015-04-21 12:19:44 -0300

    Start project.

---
+++ TP1 - Deteccion de seniales/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TP1 - Deteccion de seniales
+//
+//  Created by Gastón Montes on 4/21/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
