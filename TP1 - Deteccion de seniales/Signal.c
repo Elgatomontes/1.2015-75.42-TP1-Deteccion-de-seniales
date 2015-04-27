ec87972c2c16c100cab342608a026b65d46f9d6d
Gastón Montes <gaston.montes@hinch.as>
2015-04-27 20:04:35 -0300

    TP1 - Add Signal file.

---
+++ TP1 - Deteccion de seniales/Signal.c
@@ -0,0 +1,9 @@
+//
+//  Signal.c
+//  TP1 - Deteccion de seniales
+//
+//  Created by Gastón Montes on 4/27/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#include "Signal.h"
---
+++ TP1 - Deteccion de seniales/Signal.h
@@ -0,0 +1,14 @@
+//
+//  Signal.h
+//  TP1 - Deteccion de seniales
+//
+//  Created by Gastón Montes on 4/27/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#ifndef __TP1___Deteccion_de_seniales__Signal__
+#define __TP1___Deteccion_de_seniales__Signal__
+
+#include <stdio.h>
+
+#endif /* defined(__TP1___Deteccion_de_seniales__Signal__) */
